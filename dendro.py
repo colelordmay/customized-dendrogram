ea1e158f82a1ffa0efb04403052e7bf82cffb76a
colelordmay <colelordmay@gmail.com>
2024-10-21 08:36:03 -0700

    Updates
    Added some updates so it's in line with newer versions of matplotlib. Additionally added a couple more features and some more defaults.

--- dendro.py
+++ dendro.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.cluster.hierarchy as shc
+import matplotlib.patches as patches
+import matplotlib
 
 
 def cluster_relabel(y: np.ndarray) -> np.ndarray:
@@ -37,10 +39,12 @@ def decompose_linkage(k,Z,n_obs):
 
 def improved_dendrogram(ax: plt.axis,
                         dat: np.ndarray,
-                        n: int,
-                        p: int=20,
-                        c_list: list = ['C0','C1','C2','C3','C4','C5','C6','C7','C8','C9'],
-                        cluster_names: list = None) -> tuple[plt.axis, np.ndarray]:
+                        n_clust: int,
+                        n_leaves: int=20,
+                        c_list: list = None,
+                        cluster_names: list = None,
+                        rectangle_on = True,
+                        cmap = matplotlib.colormaps.get_cmap('Spectral')) -> tuple[plt.axis, np.ndarray]:
     
     '''Using shc and ward's method, plot a dendrogram that is more customizable and visually appealing
     
@@ -53,17 +57,23 @@ def improved_dendrogram(ax: plt.axis,
         2-dimensional numpy array to on which to cluster.
         Should be oriented such that dat.cluster = (n_observations,n_variables)
         
-    n : int
+    n_clust : int
         Desired number of clusters
         
-    p : int
+    n_leaves : int
         Number of leaves at bottom of dendrogram
         
     c_list : list
-        Colors to assign to each cluster. len(c_list) must be >= n
+        Colors to assign to each cluster. len(c_list) > n is required
         
-    cluster_names: list
+    cluster_names : list
         Names to label under each cluster
+
+    rectangle_on : bool
+        If true, plots a low alpha rectangle underneath each branch to visually highlight each cluster
+
+    cmap : LinearSegmentedColormap
+        The colormap to use if c_list is not provided
         
         
     Returns
@@ -75,11 +85,20 @@ def improved_dendrogram(ax: plt.axis,
         Array of cluster labels
 
     '''
-    
-    
+    ### This is the only error catch I've bothered to write. There are many other ways to break the code, but this is the most common.
+    if c_list:
+        if n_clust > len(c_list):
+            raise ValueError('If entering a list of colors, len(c_list) >= n_clust is required.')
+
     ### Correct inputs to reduce errors
     n_obs = dat.shape[0]
-    c_list = np.array(c_list)
+
+    if c_list:
+        c_list = np.array(c_list)
+
+    else:
+        c_list = cmap(np.linspace(0.1,0.9,n_clust))
+    
     
     ### First, cluster as normal. 
     ### We close out of the dendrogram because we're going to plot it ourselves. We just need dend
@@ -88,11 +107,11 @@ def improved_dendrogram(ax: plt.axis,
             orientation='top', 
             truncate_mode='lastp',
             count_sort='descending',
-            p=p)
+            p=n_leaves)
     plt.cla()
     
     ### Relabel the clusters such that the first one is the one with the most elements
-    clus = cluster_relabel(shc.fcluster(Z,n,criterion='maxclust')-1)
+    clus = cluster_relabel(shc.fcluster(Z,n_clust,criterion='maxclust')-1)
 
     ### Cast lists as arrays for more convenience
     y_c = np.array(dend['dcoord'])
@@ -106,13 +125,13 @@ def improved_dendrogram(ax: plt.axis,
     ### Determine what color belongs between what horizontal extent
     ### e.g. if c_lims[2] = [12.5,22.5], then all observations between these numbers should be c_list[2]
     c_lims = []
-    for i in range(n):
-        r = (5+10*np.arange(p))[CC==i]
+    for i in range(n_clust):
+        r = (5+10*np.arange(n_leaves))[CC==i]
         c_lims.append(np.array([r[0]-5,r[-1]+5]))
     c_lims = np.array(c_lims)
     
     ### Automatically determine a cutoff given number of clusters
-    cutoff = np.mean(sorted(y_c[:,1])[::-1][n-2:n])
+    cutoff = np.mean(sorted(y_c[:,1])[::-1][n_clust-2:n_clust])
 
     
     
@@ -120,8 +139,8 @@ def improved_dendrogram(ax: plt.axis,
     ax.axhline(cutoff,c="k",ls="--")
     for X,Y in zip(x_c,y_c):
 
-        color_l = c_list[:n][(X[0]>=c_lims[:,0]) & (X[0]<c_lims[:,1])][0]
-        color_r = c_list[:n][(X[-1]>=c_lims[:,0]) & (X[-1]<c_lims[:,1])][0]
+        color_l = c_list[:n_clust][(X[0]>=c_lims[:,0]) & (X[0]<c_lims[:,1])][0]
+        color_r = c_list[:n_clust][(X[-1]>=c_lims[:,0]) & (X[-1]<c_lims[:,1])][0]
         
         if all(Y<cutoff): # Color when all lines below cutoff
             ax.plot(X,Y,c=color_l)
@@ -143,15 +162,18 @@ def improved_dendrogram(ax: plt.axis,
             else:
                 ax.plot(X[2:],Y[2:],c="k")
 
+    if rectangle_on:
+        for i in range(n_clust):
+            ax.add_patch(patches.Rectangle((c_lims[i][0]+1, 0), c_lims[i][1]-c_lims[i][0]-2, cutoff*0.9, facecolor=c_list[i],alpha=0.1,zorder=-2))
     ### Formatting
     ax.set_xticks([])
     ax.set_ylabel("Intercluster distance")
-    for i in range(n):
+    for i in range(n_clust):
         if cluster_names:
             ax.text(c_lims.mean(axis=1)[i],-0.2,cluster_names[i],ha='center',va='top')
         else:
-            ax.text(c_lims.mean(axis=1)[i],-0.2,f'C{i}',ha='center',va='top')
-    ax.set_xlim(0,p*10)
+            ax.text(c_lims.mean(axis=1)[i],-0.2,f'cluster {i+1}',ha='center',va='top')
+    ax.set_xlim(0,n_leaves*10)
     ax.set_ylim(ymin=0)
     
     return ax, clus
